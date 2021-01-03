2f5640d72cd38f978b4739fddfb237f112a9ff40
AmPtUrYO <britta-exner@gmx.de>
2021-01-03 20:13:24 +0100

    .

---
+++ comments.js
@@ -0,0 +1,9 @@
+let name = document.getElementById("name").value;
+let text = document.getElementById("text").value;
+let submit = document.getElementById("submit");
+
+function comment() {
+  console.log(name);
+  console.log(text);
+  console.log(submit);
+}
