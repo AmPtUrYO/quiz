80c9985b0123ef60a25b2828238f881dfd10451a
AmPtUrYO <britta-exner@gmx.de>
2021-01-03 19:12:18 +0100

    more style

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
 console.log("verlinkt");
 
+let background1 = document.getElementById("background1");
+let background2 = document.getElementById("background2");
+let background3 = document.getElementById("background3");
+let background4 = document.getElementById("background4");
+let background5 = document.getElementById("background5");
+let background6 = document.getElementById("background6");
+let background7 = document.getElementById("background7");
+let background8 = document.getElementById("background8");
+let background9 = document.getElementById("background9");
+let background10 = document.getElementById("background10");
 
+background1.addEventListener("click", function () {
+  document.body.style.backgroundColor = "red";
+});
+
+background2.addEventListener("click", function () {
+  document.body.style.backgroundColor = "blue";
+});
+
+background3.addEventListener("click", function () {
+  document.body.style.backgroundColor = "yellow";
+});
+
+background4.addEventListener("click", function () {
+  document.body.style.backgroundColor = "green";
+});
+
+background5.addEventListener("click", function () {
+  document.body.style.backgroundColor = " rgb(210, 119, 238)";
+});
+
+background6.addEventListener("click", function () {
+  document.body.style.backgroundColor = "rgb(70, 198, 230)";
+});
+
+background7.addEventListener("click", function () {
+  document.body.style.backgroundColor = "rgb(235, 205, 74)";
+});
+
+background8.addEventListener("click", function () {
+  document.body.style.backgroundColor = " rgb(146, 53, 221)";
+});
+
+background9.addEventListener("click", function () {
+  document.body.style.backgroundColor = " rgb(255, 139, 31)";
+});
+
+background10.addEventListener("click", function () {
+  document.body.style.backgroundColor = "rgb(147, 238, 119)";
+});
