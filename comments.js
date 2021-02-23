f923cd89dd20de7d49ef2df615211ae64b5c6e72
AmPtUrYO <britta-exner@gmx.de>
2021-02-23 21:47:01 +0100

    fav

--- comments.js
+++ comments.js
@@ -1,12 +1,9 @@
-let namee = document.getElementById("namee").value;
-let text = document.getElementById("text").value;
 let submit = document.getElementById("submit");
 
 function comment() {
-  console.log(namee);
-  console.log(text);
-  console.log(submit);
+  let namee = document.getElementById("namee").value;
+  let text = document.getElementById("text").value;
   alert(
-    `Hello ${namee}, please contact me if you know how to get this to work ${text}`
+    `Hello ${namee}, please contact me if you know how to get this to work! ${text}`
   );
 }
